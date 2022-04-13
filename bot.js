bc4899714e36df70a5973f1acb1bac7648be2475
Charles Conerly <charlesconerly@lawn-128-61-113-189.lawn.gatech.edu>
2022-04-13 00:26:29 -0400

    added kevin

--- bot.js
+++ bot.js
@@ -1,3 +1,10 @@
+//git status
+//git add .
+//git commit -m "edits"
+//git push
+
+
+
 var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 
@@ -5,23 +12,65 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+  botRegex = /^\/cool guy$/;
 
-  if(request.text && botRegex.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
+  if (request.text && request.user_id != "83668014"){
+    if(request.text && botRegex.test(request.text)) {
+      this.res.writeHead(200);
+      postMessage();
+      this.res.end();
+    } else {
+      console.log("don't care");
+      this.res.writeHead(200);
+      this.res.end();
+    }
   }
+  else {
+   this.res.writeHead(200);
+   postMessagetwo(bartroast());
+   this.res.end();  
+ }
 }
 
 function postMessage() {
   var botResponse, options, body, botReq;
 
   botResponse = "work";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postMessagetwo() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "Hey Kevin";
 
   options = {
     hostname: 'api.groupme.com',
