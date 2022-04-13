ec4ce115577fe5c038ddacf6db4f7100509bd8db
Charles Conerly <charlesconerly@lawn-128-61-113-189.lawn.gatech.edu>
2022-04-13 00:33:03 -0400

    added kevin

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
   botRegex = /^\/cool guy$/;
 
-  if (request.text && request.user_id != "83668014"){
+  if (request.text && request.user_id != "17996985"){
     if(request.text && botRegex.test(request.text)) {
       this.res.writeHead(200);
       postMessage();
