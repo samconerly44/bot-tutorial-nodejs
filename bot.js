c283a73b38b23c92c4ffb12ae66e6d8cdd8424a3
Charles Conerly <charlesconerly@lawn-128-61-113-189.lawn.gatech.edu>
2022-04-13 01:19:10 -0400

    photo

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ function respond() {
   if (request.text && request.user_id != "17996985"){
     if(request.text && botRegex.test(request.text)) {
       this.res.writeHead(200);
-      postMessage();
+      postMessageThree();
       this.res.end();
     } else {
       console.log("don't care");
@@ -35,7 +35,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "work";
+  botResponse = "";
 
   options = {
     hostname: 'api.groupme.com',
@@ -102,5 +102,43 @@ function postMessagetwo() {
   botReq.end(JSON.stringify(body));
 }
 
+function postMessageThree() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "😹😹😹";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    'bot_id': botID,
+    'text': botResponse,
+    'attachments': [{
+      'type': 'image',
+      'url': "https://drive.google.com/file/d/1MYFrdhiKgP-OEd9kCxBbvPrQzy50O9BM/view?usp=sharing"
+    }]
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 
 exports.respond = respond;
